304c932cedd1bf9bd97fb2436c84d03e6550c9cd
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-23 11:54:51 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import WarehouseGame from './components/WarehouseGame'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50">
+      <WarehouseGame />
+    </main>
+  )
+}
